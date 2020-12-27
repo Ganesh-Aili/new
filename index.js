47bed44a87f46ddafbcf4e3a0413b074f7c1c350
Ganesh-Aili <75843885+Ganesh-Aili@users.noreply.github.com>
2020-12-27 18:18:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+const port = process.env.PORT;
+const database = require('./database');
+const userController = require('./controller/user');
+
+app.use(morgan('dev'));
+
+app.use(cors());
+
+app.use('/api/user', userController);
+
+
+app.all(
+    '/',
+    function(req, res) {
+        return res.json({
+            status: true,
+            message: 'Index page working....'
+        });
+    }
+);
+
+app.listen(port,
+    function() {
+
+        console.log('server running in port:' + port);
+
+    });
